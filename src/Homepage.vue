17673467d162e00fbd246147bf118c3bb74e3439
ianian4302 <88609092+ianian4302@users.noreply.github.com>
2024-06-29 04:08:05 +0800

    chore: Add Homepage and MainPage components
    The Homepage component is added to display the main content of the website, including a photo, contact information, and a skill list. The MainPage component is created as the main layout for the website, including a header, the Homepage component, and a sidebar.

---
+++ src/Homepage.vue
@@ -0,0 +1,61 @@
+<script setup>
+import ContactInfoBox from "./components/ContactInfoBox.vue";
+import SkillList from "./components/SkillList.vue";
+</script>
+
+<template>
+    <body>
+        <main>
+            <header></header>
+            <div class="content">
+                <div class="photo">
+                    <img src="@/assets/your-photo.jpg" alt="Your Photo" />
+                </div>
+                <ContactInfoBox />
+                <div class="skill"><SkillList /></div>
+            </div>
+        </main>
+    </body>
+</template>
+
+<style scoped>
+body{
+    
+}
+main {
+    min-height: 100vh;
+    background: url("./assets/background_1.png") no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: multiply;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+}
+
+.photo {
+    margin-bottom: 20px;
+    filter: drop-shadow(0 0 15px rgba(0, 0, 0, 0.5));
+}
+
+.photo img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+
+.skill {
+    margin-top: 5%;
+}
+</style>
---
+++ src/MainPage.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Sidebar from "./components/SideBar.vue";
+import HomePage from "./HomePage.vue";
+
+import Test from "./components/Test.vue";
+</script>
+<script></script>
+
+<template>
+    <header></header>
+    <body>
+        <main>
+            <HomePage />
+            <!-- <Test /> -->
+            <Sidebar />
+        </main>
+    </body>
+</template>
+
+<style scoped>
+body {
+ 
+}
+main {
+  
+}
+</style>
