79aefa5ba467557d204c1cc632cc399c2d369b02
ianian4302 <88609092+ianian4302@users.noreply.github.com>
2024-06-29 04:07:50 +0800

    feat: Add ContactInfoBox component for displaying contact information
    The new ContactInfoBox component is added to display contact information such as name, phone number, and email. The component is styled with a centered layout, white text color, and a blurred background.

---
+++ src/components/ContactInfoBox.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="contact_field">
+        <div class="conf_field">
+            <div class="conf_inf">陳文彥 WEN-YEN CHEN</div>
+            <div class="conf_inf">phone : 0966284266</div>
+            <div class="conf_inf">email : ianian4302@gmail.com</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.contact_field {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 500;
+    white-space: nowrap;
+    margin: 0 20% 0;
+    border-radius: 10px;
+    height: 10%;
+    backdrop-filter: blur(2px);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 1);
+    padding: 60px 150px;
+}
+.conf_inf {
+    color: white;
+}
+</style>
